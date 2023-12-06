3062e518d0a7dbc9641caa4ee5666ecd16d9e2ee
YayL <jonasbeast9@gmail.com>
2023-12-06 10:46:33 +0100

    Fixed week 48?

--- Veckouppgifter/Kursvecka 48 - (v4)/installer.bash
+++ Veckouppgifter/Kursvecka 48 - (v4)/installer.bash
@@ -4,6 +4,7 @@ which_result=$(command -v $1)
 
 if [ -z $which_result ]; then
     echo "Installning $1"
+    #yay -Syu $1
     sudo apt-get update
     sudo apt-get install $1
 fi
@@ -12,15 +13,16 @@ echo "Running $2 processes of $1 for $3 seconds in xterm"
 
 for i in $(seq 1 $2)
 do
-    2>/dev/null 1>&2 eval $1 &
+    2>/dev/null 1>&2 xterm -e "$1" &
 done
 sleep $3
 
-pgrep -P $$ --signal SIGTERM
+pgrep -P $$ | xargs kill
 
 echo -n "Uninstall $1 ? (y/n): "
 read val
 if [[ "$val" == "y" ]]; then
     echo "Uninstallning $1"
+    #yay -R $1
     sudo apt-get uninstall $1
 fi
