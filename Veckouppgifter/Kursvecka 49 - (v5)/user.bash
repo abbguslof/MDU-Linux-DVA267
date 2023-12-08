bf3394ddfd7c2b6df8c34fda6c94c557f77a96bd
abbguslof <gustav.lofqvist@abbgymnasiet.se>
2023-12-08 11:00:00 +0100

    started week 5 bash

---
+++ Veckouppgifter/Kursvecka 49 - (v5)/user.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -u|--users) USERS=true; shift;;
+    -o|--online) ONLINE=true; shift;;
+    --help) HELP=true; VERSION=; IP=; MAC=; break;;
+    *) shift;;
+  esac
+done
+
+if [ "$HELP" = true ]; then
+    echo "Usage: $(basename "$0") OPTION..."
+    echo -e "Print users\n\nOPTIONS"
+    echo -e "-u, --users,\t\tList all user accounts\n"\
+            "-o, --online\t\tList all online users\n"\
+            "    --help\t\tDisplay this help and exit"
+fi
+
+if [ "$USERS" = true ]; then
+    cut -d: -f1 /etc/passwd | column
+fi
+
+if [ "$ONLINE" = true ]; then
+    cut -d: -f1 /etc/passwd | column
+fi
